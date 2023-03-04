5874b83b9bc92d552674be9fb8ea4d03a8f3447f
Fikerte-T <tofikertesfaye@gmail.com>
2023-03-05 00:43:41 +0300

    Create components, store, and reducer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,8 @@
 // import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
 
 ReactDOM.render(
   <App />,
